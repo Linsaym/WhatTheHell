ad6adba0baec383f93fb3e8dcc28d5f5391bd1fa
Linsaym <leva397@mail.ru>
2024-11-02 08:22:21 +0300

    update buffs

--- resources/js/Pages/Guides/bosses/Buffs.vue
+++ resources/js/Pages/Guides/bosses/Buffs.vue
@@ -25,19 +25,10 @@ import {Badge} from "@/Components/ui/badge/index.js";
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <div>
-                            <h3 class="text-3xl font-bold mb-6">Вступление</h3>
+                            <h3 class="text-3xl font-bold mb-6">Гайд по баферству на боссах</h3>
                             <div class="space-y-4">
-                                <p class="text-lg">
-                                    С каждого босса дают только 4 сундука. Эти сундуки получают игроки, которые нанесли
-                                    наибольшее количество урона.
-                                </p>
                                 <div class="text-lg space-y-4">
-                                    <p>Чтобы нанести максимум урона, собирается пачка из 4 игроков.</p>
-                                    <p class="text-green-600 italic">
-                                        //TODO Это нужно стилизовать и оптимизировать,
-                                        добавить картинки и так далее
-                                    </p>
-                                    <p>
+                                    <p class="text-xl">
                                         Здесь мы научим вас азам баферского ремесла: зачем оно нужно и как помогает.
                                         Дроп с боссов и элиток получают только четыре игрока с наибольшим уроном.
                                         Исключение составляют ПВП боссы и Зибо, но это не сюда. Даже самый сильный ДД не
@@ -48,34 +39,91 @@ import {Badge} from "@/Components/ui/badge/index.js";
                                         Сейчас обсудим их более детально, а о талантах вы можете узнать в гайдах своего
                                         класса.
                                     </p>
-                                    <p>
+                                    <div>
                                         Список баферских скилов:
-                                        <ul class="pl-8 list-disc">
-                                            <li>Мишень(охотник, снайпер)</li>
-                                            <li>Поход(охотник, снайпер)</li>
-                                            <li>Воодушевление(тр, крест)</li>
-                                            <li>Флаг ТР-а</li>
-                                            <li>Флаг креста (нужен талант)</li>
+                                        <ul>
+                                            <li class="flex gap-3 items-center my-3">
+                                                <a href="https://royalquest.info/index.php/Мишень"
+                                                   target="_blank"
+                                                >
+                                                    <img
+                                                        src="https://royalquest.info/images/0/0c/Умение_Мишень.png"
+                                                        style="width: 48px"
+                                                        alt="Мишень">
+                                                </a>
+                                                <p class="w-2/3">
+                                                    - Мишень (охотник, снайпер). Увеличивает урон дальников которые
+                                                    находятся в группе
+                                                </p>
+                                            </li>
+                                            <li class="flex gap-3 items-center my-3">
+                                                <a href="https://royalquest.info/index.php/Поход"
+                                                   target="_blank"
+                                                >
+                                                    <img
+                                                        src="https://royalquest.info/images/2/27/Умение_Поход.png"
+                                                        style="width: 48px"
+                                                        alt="Поход">
+                                                </a>
+                                                <p class="w-2/3">
+                                                    - Поход (охотник, снайпер). Увеличивает скорость атаки всех челенов
+                                                    группы
+                                                </p>
+                                            </li>
+                                            <li class="flex gap-3 items-center my-3">
+                                                <a href="https://royalquest.info/index.php/Воодушевление"
+                                                   target="_blank"
+                                                >
+                                                    <img
+                                                        src="https://royalquest.info/images/3/3d/Умение_Воодушевление.png"
+                                                        style="width: 48px"
+                                                        alt="Воодушевление">
+                                                </a>
+                                                <p>
+                                                    - Воодушевление. Накладывает эффект "Мощь (х3)" (Увеличивает атаку
+                                                    на 6%). Может стакаться до 10 эффектов
+                                                </p>
+                                            </li>
+                                            <li class="flex gap-3 items-center my-3">
+                                                <a href="https://royalquest.info/index.php/Штандарт_тьмы"
+                                                   target="_blank"
+                                                >
+                                                    <img
+                                                        src="https://royalquest.info/images/5/51/Умение_Штандарт_тьмы.png"
+                                                        style="width: 48px"
+                                                        alt="Воодушевление">
+                                                </a>
+                                                <p>
+                                                    - Штандарт Тьмы (Тёмный рыцарь). Союзники наносят повышенный на 8%
+                                                    урон.
+                                                </p>
+                                            </li>
+                                            <li class="flex gap-3 items-center my-3">
+                                                <a href="https://royalquest.info/index.php/Знамя_света"
+                                                   target="_blank"
+                                                >
+                                                    <img
+                                                        src="https://royalquest.info/images/b/b2/Умение_Знамя_света.png"
+                                                        style="width: 48px"
+                                                        alt="Воодушевление">
+                                                </a>
+                                                <p>
+                                                    - Знамя света (крестоносец). С талантом
+                                                    <a class="text-blue-500"
+                                                       href="https://royalquest.info/index.php/Талант_—_Воин_света"
+                                                       target="_blank">
+                                                        Воин света
+                                                    </a> увеличивает атаку союзников на 7%
+                                                </p>
+                                            </li>
                                         </ul>
-                                    </p>
+                                    </div>
                                     <p>
                                         Задача баферов по очереди давать скилы, так чтобы эффекты не спадали.
-                                        Таким образом увеличивая урон своей группы, что позволяет ДД-шникам забрать сундуки
+                                        Таким образом увеличивая урон своей группы, что позволяет ДД-шникам забрать
+                                        сундуки, нанося по боссу наибольшее количество урона.
                                     </p>
-                                    <p class="space-y-2">
-                                        <a href="https://royalquest.info/index.php/Мишень" target="_blank"
-                                           class="text-blue-600 underline">
-                                            Мишень:
-                                        </a> Указанный противник получает метку на 15 сек., увеличивая урон от дальних
-                                        атак на 15%.
-                                        <br>
-                                        <a href="https://royalquest.info/index.php/Клеймо_еретика" target="_blank"
-                                           class="text-blue-600 underline">
-                                            Клеймо еретика:
-                                        </a> Помечает противника на 12 сек., повышая урон от умений и обычных атак на
-                                        25%.
-                                    </p>
-                                    <p class="space-y-2">
+                                    <div class="space-y-2 text-xl">
                                         Также есть навыки, которые не бафают урон напрямую, но помогают в сражениях:
                                         <br>
                                         <a href="https://royalquest.info/index.php/Надёжная_защита" target="_blank"
@@ -92,7 +140,7 @@ import {Badge} from "@/Components/ui/badge/index.js";
                                            class="text-blue-600 underline">
                                             Жертва
                                         </a> — также снижает получаемый урон.
-                                    </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
